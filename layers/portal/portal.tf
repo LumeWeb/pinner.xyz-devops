7cffb049552e00af953c4537efaddcde7102723f
Derrick Hammer <derrick@derrickhammer.com>
2025-01-07 10:59:00 -0500

    chore: move portal to standard-next-next

--- layers/portal/portal.tf
+++ layers/portal/portal.tf
@@ -28,7 +28,7 @@ module "portal" {
   count = local.portal_instance_count
 
   name  = "pinner-portal"
-  image = "ghcr.io/lumeweb/akash-portal:standard-latest"
+  image = "ghcr.io/lumeweb/akash-portal:standard-next-next"
 
   domain      = local.base_domain
   portal_name = var.portal_name
